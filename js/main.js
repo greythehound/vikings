374cb4d542a22ef1468c9e27e41addda97e11025
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-10-10 22:14:43 +0300

    finalizing with adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function(){
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
 
     $('.heroes_slider-img').slick({
         slidesToShow: 1,
@@ -7,6 +11,16 @@ $(function(){
         asNavFor: '.heroes__slider-text',
         prevArrow: '<button class="slick-btn slick-prev"><img src="images/slider-arrow-left.png" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next"><img src="images/slider-arrow-right.png" alt="next"></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+              }
+            },
+        ],
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
