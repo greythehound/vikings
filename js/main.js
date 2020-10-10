b66ebe11871ebf0ef5c5318c4a257e7c884175ca
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-10-10 15:35:08 +0300

    adding seasons-section, slider, season-1 page, episodes, linking pages together

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(function(){
-    $('[data-fancybox]').fancybox({
-        youtube : {
-            controls : 0,
-            showinfo : 0
-        },
+
+
+    $('.heroes_slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/slider-arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/slider-arrow-right.png" alt="next"></button>',
     });
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes_slider-img',
+        fade: true,
+        arrows: false,
+      });
 });
